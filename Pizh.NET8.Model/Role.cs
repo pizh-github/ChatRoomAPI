aebfec4ad557d9966f340a56f430d6e9fa6b2c7b
pizh-github <pizuhu@126.com>
2024-09-18 16:25:22 +0800

    clear

--- Pizh.NET8.Model/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pizh.NET8.Model
-{
-    public class Role
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-    }
-}
--- Pizh.net8.Common/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Pizh.net8.Common
-{
-    public class Class1
-    {
-
-    }
-}
